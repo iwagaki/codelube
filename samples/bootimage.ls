4ced77f7359a16c9aa1a5a49ca5f3ee38750f6e4
iwagaki <iwagaki@users.sourceforge.net>
2010-07-10 16:22:46 +0900

    added a second loader to load a program with over MBR size

--- samples/bootimage.ls
+++ samples/bootimage.ls
@@ -1,12 +1,33 @@
 OUTPUT_FORMAT("binary");
-BASE_ADDR = 0;
-SIGN_ADDR = 510;
 
 SECTIONS
 {
-    . = BASE_ADDR;
+    . = 0;
     .boot_loader : {
     	ipl.o
+    }
+    . = 446;
+    .partition_table : {
+        BYTE(0x80) /* boot flag */
+        BYTE(0x00) /* start CHS  H */
+        BYTE(0x01) /* start CHS  [5:0] -> S, [6:7] -> C[8:9] */
+        BYTE(0x00) /* start CHS  C[0:7] */
+        BYTE(0x0c) /* partition type */
+        BYTE(0x00) /* end CHS (TBD) */
+        BYTE(0x3f)
+        BYTE(0x00)
+	SHORT(0x0000) /* start LBA */
+	SHORT(0x0000)
+	SHORT(0x003f) /* end LBA (TBD) */
+	SHORT(0x0000)
+    }
+    . = 510;
+    .boot_signature : {
+        SHORT(0xaa55)
+    }
+    . = 512;
+    .kernel : { /* should be placed here */
+        kernel.o
     }
     .text : {
         *(.text)
@@ -19,9 +40,5 @@ SECTIONS
     }
     .bss : {
         *(.bss)
-    }
-    . = SIGN_ADDR;
-    .boot_signature : {
-        SHORT(0xaa55)
     }
 }
--- samples/main.cc
+++ samples/main.cc
@@ -5,30 +5,31 @@ __asm__(".code16gcc"); // リアルモード用のコード
 extern "C"
 {
     extern void main_func(); // マングリングしないようにする
+    extern void error_func(); // マングリングしないようにする
 }
 
 class VRAMUtil
 {
 public:
-    VRAMUtil() : m_pos(0) {}
+    void putChar(uint8_t x, uint8_t y, uint16_t ch)
+    {
+        writeChar(xyToAddr(x, y), ch);
+    }
 
-    void putString(uint8_t* str)
+    void putString(uint8_t x, uint8_t y, uint8_t* str)
     {
         int ch = 0;
         while ((ch = *str++) != 0)
-        {
-            putChar(m_pos, ch);
-            m_pos += 2;
-        }
-    }
-
-    void pos(uint32_t pos)
-    {
-        m_pos = pos;
+            writeChar(xyToAddr(x++, y), ch | 0x8300);
     }
 
 private:
-    void putChar(uint32_t addr, uint32_t ch)
+    uint32_t xyToAddr(uint8_t x, uint8_t y)
+    {
+        return (y * 80 + x) * 2;
+    }
+
+    void writeChar(uint32_t addr, uint32_t ch)
     {
         __asm__(
             "push %%es;"
@@ -37,8 +38,8 @@ private:
             "push %%dx;"
             "movw $0xb800, %%dx;"
             "movw %%dx, %%es;"
-            "movb $0xec, %%ah;"
-            "movw %%ax, %%es:2(%%bx);"
+            // "movb $0xec, %%ah;"
+            "movw %%ax, %%es:(%%bx);"
             "pop %%dx;"
             "pop %%bx;"
             "pop %%ax;"
@@ -47,16 +48,43 @@ private:
             : "b"(addr), "a"(ch)
             );
     }
-
-    uint32_t m_pos;
 };
+
+void error_func()
+{
+    uint8_t error_msg[] = "Error!";
+    VRAMUtil vram;
+    vram.putString(0, 0, error_msg);
+}
 
 void main_func()
 {
-    uint8_t asciiz[] = "hello,world!";
-
     VRAMUtil vram;
 
-    vram.pos(0);
-    vram.putString(asciiz);
+    for (uint8_t h = 0; h < 25; ++h)
+    {
+        for (int8_t w = 0; w < 80; ++w)
+        {
+            if ((h == 0 || h == 24) || (w == 0 || w == 79))
+                vram.putChar(w, h, 0x6020);
+            if (((h % 3 == 2) && h < 12) && ((w % 10) > 1 && (w % 10) < 8))
+                vram.putChar(w, h, 0x2020);
+        }
+    }
+
+    int8_t x = 10, y = 10, vx = 1, vy = 1;
+    
+    for (;;)
+    {
+        vram.putChar(x, y, 0x0020);
+        x += vx; y += vy;
+        for (int i = 0; i < 1000000; ++i)
+            vram.putChar(x, y, 0x3020);
+
+        if (x == 1 || x == 78)
+            vx = -vx;
+
+        if (y == 1 || y == 23)
+            vy = -vy;
+    }
 }
