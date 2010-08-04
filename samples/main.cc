7a1cc644aba0b22d05cc327bd094a979ce617604
iwagaki <iwagaki@users.sourceforge.net>
2010-08-05 00:31:11 +0900

    fixed a bug that symbols in .data and .bss are not linked properly

--- samples/main.cc
+++ samples/main.cc
@@ -16,7 +16,7 @@ public:
         writeChar(xyToAddr(x, y), ch);
     }
 
-    void putString(uint8_t x, uint8_t y, uint8_t* str)
+    void putString(uint8_t x, uint8_t y, const char* str)
     {
         int ch = 0;
         while ((ch = *str++) != 0)
@@ -52,39 +52,146 @@ private:
 
 void error_func()
 {
-    uint8_t error_msg[] = "Error!";
+    char error_msg[] = "Error!";
     VRAMUtil vram;
     vram.putString(0, 0, error_msg);
 }
 
-void main_func()
+uint8_t mapData[][18] = {
+    {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2},
+    {2, 7, 2, 7, 2, 7, 2, 7, 2, 7, 2, 7, 2, 7, 2, 7, 2, 7},
+    {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2},
+    {2, 7, 2, 7, 2, 7, 2, 7, 2, 7, 2, 7, 2, 7, 2, 7, 2, 7},
+    {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2},
+    {2, 7, 2, 7, 2, 7, 2, 7, 2, 7, 2, 7, 2, 7, 2, 7, 2, 7},
+    {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2},
+    {2, 7, 2, 7, 2, 7, 2, 7, 2, 7, 2, 7, 2, 7, 2, 7, 2, 7},
+    {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2},
+    {2, 7, 2, 7, 2, 7, 2, 7, 2, 7, 2, 7, 2, 7, 2, 7, 2, 7},
+};
+
+
+void drawScreen()
 {
     VRAMUtil vram;
 
+    // 枠
     for (uint8_t h = 0; h < 25; ++h)
     {
         for (int8_t w = 0; w < 80; ++w)
         {
-            if ((h == 0 || h == 24) || (w == 0 || w == 79))
+            if ((h == 1 || h == 24) || ((h > 0) && (w == 0 || w == 79)))
                 vram.putChar(w, h, 0x6020);
-            if (((h % 3 == 2) && h < 12) && ((w % 10) > 1 && (w % 10) < 8))
-                vram.putChar(w, h, 0x2020);
         }
     }
 
-    int8_t x = 10, y = 10, vx = 1, vy = 1;
+    // スコア
+    vram.putString(0, 0, "SCORE: 0");
+
+    // 面表示
+    for (uint8_t h = 0; h < 10; ++h)
+    {
+        for (int8_t w = 0; w < 18; ++w)
+        {
+            if (mapData[h][w] != 0)
+            {
+                uint16_t col = 0x0020 | (mapData[h][w] << 12);
+                vram.putChar(w * 4 + 4 + 0, h + 3, col);
+                vram.putChar(w * 4 + 4 + 1, h + 3, col);
+                vram.putChar(w * 4 + 4 + 2, h + 3, col);
+                vram.putChar(w * 4 + 4 + 3, h + 3, col);
+            }
+        }
+    }
+}
+
+bool isBlick(uint8_t x, uint8_t y)
+{
+    if ((x >= 4) && (x <= 17 * 4 + 4 + 3) && (y >= 3) && (y <= 9 + 3))
+        return (mapData[y - 3][(x - 4) / 4] != 0);
+
+    return false;
+}
+
+void clearBlick(uint8_t x, uint8_t y)
+{
+    VRAMUtil vram;
+
+    if ((x >= 4) && (x <= 17 * 4 + 4 + 3) && (y >= 3) && (y <= 9 + 3))
+    {
+        uint8_t w = (x - 4) / 4;
+        uint8_t h = y - 3;
+        mapData[h][w] = 0;
+        vram.putChar(w * 4 + 4 + 0, h + 3, 0x0020);
+        vram.putChar(w * 4 + 4 + 1, h + 3, 0x0020);
+        vram.putChar(w * 4 + 4 + 2, h + 3, 0x0020);
+        vram.putChar(w * 4 + 4 + 3, h + 3, 0x0020);
+    }
+}
+
+
+void showScore(int score)
+{
+    VRAMUtil vram;
+
+    char string[] = "SCORE: 0000";
+
+    int u1000 = score / 1000;
+    int u100 =  (score % 1000) / 100;
+    int u10 =  (score % 100) / 10;
+    int u1 =  score % 10;
+
+    string[7] = '0' + u1000;
+    string[8] = '0' + u100;
+    string[9] = '0' + u10;
+    string[10] = '0' + u1;
+
+    vram.putString(0, 0, string);
+}
+
+
+void main_func()
+{
+
+    VRAMUtil vram;
+    drawScreen();
+
+    int score = 0;
+
+    int8_t x = 10, y = 20, vx = 1, vy = 1;
     
     for (;;)
     {
-        vram.putChar(x, y, 0x0020);
-        x += vx; y += vy;
-        for (int i = 0; i < 1000000; ++i)
-            vram.putChar(x, y, 0x3020);
+        showScore(score);
 
-        if (x == 1 || x == 78)
+        vram.putChar(x, y, 0x0020);
+
+        int8_t xx = x + vx;
+        int8_t yy = y + vy;
+
+        if (xx == 0 || xx == 79)
             vx = -vx;
 
-        if (y == 1 || y == 23)
+        if (yy == 1 || yy == 24)
             vy = -vy;
+
+        if (isBlick(xx, y))
+        {
+            clearBlick(xx, y);
+            vx = -vx;
+            score += 10;
+        }
+
+        if (isBlick(x, yy))
+        {
+            clearBlick(x, yy);
+            vy = -vy;
+            score += 10;
+        }
+
+        x = x + vx; y = y + vy;
+
+        for (int i = 0; i < 100000; ++i)
+            vram.putChar(x, y, 0x3020);
     }
 }
